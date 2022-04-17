aa695b754028441fef0b24d74dfa6ff40d55b821
Kawsar Ahmed <coder.kawsar@gmail.com>
2022-04-17 13:28:10 +0600

    components created, header done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Blog from "./components/Blog/Blog";
+import About from "./components/About/About";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Services/Service.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Services.css";
+
+const Service = () => {
+  return (
+    <div>
+      <h2>Services</h2>
+    </div>
+  );
+};
+
+export default Service;
